222c57b27a94d68b8fbc7025f08a48ba1c6e2ef0
Akshik Tumma <atumma@iastate.edu>
2015-10-14 14:36:54 -0500

    Added standard intro header

--- mod_form.php
+++ mod_form.php
@@ -48,8 +48,7 @@ class mod_certificate_mod_form extends moodleform_mod {
         }
         $mform->addRule('name', null, 'required', null, 'client');
 
-        $this->add_intro_editor(false, get_string('intro', 'certificate'));
-
+	$this->standard_intro_elements(get_string('intro', 'certificate'));
         // Issue options
         $mform->addElement('header', 'issueoptions', get_string('issueoptions', 'certificate'));
         $ynoptions = array( 0 => get_string('no'), 1 => get_string('yes'));
